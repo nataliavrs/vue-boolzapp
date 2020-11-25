966ce57697a20d4439289c7af1ec5b2baf719c0a
nataliavrs <verasnatali4@gmail.com>
2020-11-25 17:19:32 +0100

    Seeing chat that matches full input value

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 var app = new Vue({
   el: "#app",
   data: {
+    userName: "Natália Veras",
+
     inputMessage: "",
     openClass: "",
     indexChat: 0,
+
+    todayDate: new Date (),
+    nowHour: new Date (),
+
+    searchInput: '',
+
+
     contactsList: [
     {
-      name: 'Michele',
+      name: 'michele',
       contactPicture: 'img/avatar_1.jpg',
       allMessages: [
         {
@@ -27,8 +36,8 @@ var app = new Vue({
       ]
     },
     {
-     name: 'Rachele',
-     contactPicture: 'img/avatar_2.jpg',
+     name: 'rachele',
+     contactPicture: 'img/avatar_6.jpg',
      allMessages: [
        {
          message: "Ciao!",
@@ -48,21 +57,21 @@ var app = new Vue({
      ]
     },
     {
-    name: 'Matteo',
+    name: 'matteo',
     contactPicture: 'img/avatar_3.jpg',
     allMessages: [
       {
-        message: "ciao",
+        message: "Let's hang out later?",
         origin: "sent",
         hour: "12:09"
       },
       {
-        message: "sto bene",
+        message: "Uhmm, where?",
         origin: "received",
         hour: "08:09"
       },
       {
-        message: "interessante",
+        message: "I don't know, let me think...",
         origin: "sent",
         hour: "10:39"
       }
@@ -73,52 +82,117 @@ var app = new Vue({
      contactPicture: 'img/avatar_4.jpg',
      allMessages: [
        {
-         message: "ciao",
+         message: "Mamma mia, che dire",
          origin: "sent",
          hour: "05:56"
        },
        {
-         message: "sto bene",
+         message: "Che ne so",
          origin: "received",
          hour: "18:12"
        },
        {
-         message: "interessante",
+         message: "Va bene, dai",
          origin: "sent",
          hour: "15:09"
        }
      ]
     },
     {
-    name: 'Mattia',
-    contactPicture: 'img/avatar_5.jpg',
+    name: 'Antonio',
+    contactPicture: 'img/avatar_2.jpg',
     allMessages: [
       {
-        message: "ciao",
+        message: "Beh, o que dizer",
         origin: "sent",
         hour: "12:19"
       },
       {
-        message: "sto bene",
+        message: "Sono un lumacoide",
         origin: "received",
         hour: "14:04"
       },
       {
-        message: "interessante",
+        message: "Certo che sì",
         origin: "sent",
         hour: "18:10"
       }
     ]
-   }
+  },
+    {
+  name: 'Antonio',
+  contactPicture: 'img/avatar_2.jpg',
+  allMessages: [
+    {
+      message: "Beh, o que dizer",
+      origin: "sent",
+      hour: "12:19"
+    },
+    {
+      message: "Sono un lumacoide",
+      origin: "received",
+      hour: "14:04"
+    },
+    {
+      message: "Certo che sì",
+      origin: "sent",
+      hour: "18:10"
+    }
+  ]
+  },
+    {
+  name: 'Antonio',
+  contactPicture: 'img/avatar_2.jpg',
+  allMessages: [
+    {
+      message: "Beh, o que dizer",
+      origin: "sent",
+      hour: "12:19"
+    },
+    {
+      message: "Sono un lumacoide",
+      origin: "received",
+      hour: "14:04"
+    },
+    {
+      message: "Certo che sì",
+      origin: "sent",
+      hour: "18:10"
+    }
+  ]
+  },
+    {
+  name: 'Antonio',
+  contactPicture: 'img/avatar_2.jpg',
+  allMessages: [
+    {
+      message: "Beh, o que dizer",
+      origin: "sent",
+      hour: "12:19"
+    },
+    {
+      message: "Sono un lumacoide",
+      origin: "received",
+      hour: "14:04"
+    },
+    {
+      message: "Certo che sì",
+      origin: "sent",
+      hour: "18:10"
+    }
+  ]
+  },
     ]
   },
   methods: {
+    // SHOW CLICKED CHAT
      openChat: function(index) {
 
        this.indexChat = index;
        this.openClass = "opened";
 
      },
+     // SEND MESSAGE & AUTOMATIC ANSWER
      sendMessage: function() {
 
        if (this.inputMessage == "") {
@@ -129,44 +203,66 @@ var app = new Vue({
            {
              message: this.inputMessage,
              origin: "sent",
-             hour: "12:09"
+             hour: this.currentHour()
            }
-
          );
 
          this.inputMessage = "";
 
          setTimeout(() => this.contactsList[this.indexChat].allMessages.push (
            {
-           message: 'ok...',
+           message: 'Sì, hai ragione secondo me. Ci penserò, grazie!',
            origin: 'received',
-           hour: '12:10'
+           hour: this.currentHour()
          }), 1500);
 
        }
-     }
+     },
+     // FILTER
+     filterContacts: function() {
+
+      // const filter = this.contactsList.filter((element) => {
+      //   return element.name === this.searchInput;
+      // });
+
+
+      console.log(filter);
+      console.log(this.searchInput);
+
+     },
+     // CURRENT HOUR
+     currentHour: function() {
+
+       let seconds = this.nowHour.getSeconds();
+       if (seconds < 10) {
+         seconds = "0" + seconds;
+       }
+       let hour = this.nowHour.getHours()
+       let minute = this.nowHour.getMinutes()
+       if (minute < 10) {
+         minute = "0" + minute;
+       }
+
+       return `${hour}:${minute}:${seconds}`
+
+     },
+     // CURRENT DAY
+     currentDay: function() {
+
+       let day = this.todayDate.getDate();
+       let month = this.todayDate.getMonth() + 1
+       let year = this.todayDate.getFullYear()
+
+       return `${day}/${month}/${year}`
+
+     },
   },
+  // SCROLL TO LAST MESSAGE
   updated: function () {
 
     var container = document.querySelector(".chat-body");
     var scrollHeight = container.scrollHeight;
     container.scrollTop = scrollHeight;
-
-  },
-  // CURRENT HOUR
-  hour: function() {
-
-    let seconds = this.nowHour.getSeconds();
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-    }
-    let hour = this.nowHour.getHours()
-    let minute = this.nowHour.getMinutes()
-    if (minute < 10) {
-      minute = "0" + minute;
-    }
-
-    return `${hour}:${minute}:${seconds}`
 
   },
 
